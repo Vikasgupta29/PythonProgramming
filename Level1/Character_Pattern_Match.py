300f201189946a4411fb30e0a22c68bf67412487
Vikas Gupta <92180754+Vikasgupta29@users.noreply.github.com>
2024-05-24 23:39:22 +0530

    Create Character_Pattern_Match.py

---
+++ Level1/Character_Pattern_Match.py
@@ -0,0 +1,26 @@
+"""
+Write a python program which displays the count of the names that matches a given pattern from a list of names provided.
+Consider the pattern characters to be:
+1. "_ at" where "_" can be one occurrence of any character.
+2. "%at%" where "%" can have zero or any number of occurrences of a character.
+Sample Input:
+[Hat, Cat, Rabbit, Matter]
+Expected Output:
+_at -> 2
+%at% -> 3
+"""
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    for name in name_list:
+        if name[1:]=="at":
+            count1+=1
+        if "at" in name:
+            count2+=1
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+
+#Provide different names in the list and test your program
+name_list=["Hat","Cat","rabbit","matter"]
+count_names(name_list)
